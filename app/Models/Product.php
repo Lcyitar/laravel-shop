3020491bbfbe098307fc0ee39f9f56a15fc57ad9
lcyitar <17607004519@163.com>
2019-04-29 08:59:41 +0000

    商品属性模型

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -46,4 +46,13 @@ class Product extends Model
     {
         return $this->hasMany(ProductProperty::class);
     }
+
+    public function getGroupedPropertiesAttribute()
+    {
+        return $this->properties
+            ->groupBy('name')
+            ->map(function ($properties) {
+               return $properties->pluck('value')->all();
+            });
+    }
 }
